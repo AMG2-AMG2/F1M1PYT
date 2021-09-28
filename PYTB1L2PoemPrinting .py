3d7bad0ffdcdad365e715231d4a1732e603df5ef
Halil Dogan <33289@ma-web.nl>
2021-09-28 10:13:38 +0200

    verslag

---
+++ PYTB1L2PoemPrinting .py
@@ -0,0 +1,7 @@
+vers1 = "Mijn naam is Halil"
+vers2 = "Ik ben goed in wheelies doen"
+vers3 = "Ik hou er van om games te spellen"
+vers4 = "En ben bijna altijd buiten aan het chillen"
+vers5 = "Met me matties"
+
+print (vers1,vers2,vers3,vers4,vers5)
