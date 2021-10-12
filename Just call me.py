88bbb798cf16c38b6b07d83da6f20819311f43f1
Halil Dogan <33289@ma-web.nl>
2021-10-12 10:29:20 +0200

    Nieuwe bestand

---
+++ Just call me.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+def callMe(name, number):
+    print("&: Hello?")
+    time.sleep(1)
+    print("&: Wie is daar?")
+    time.sleep(1)
+    print("#: Ik ben het"+ Halil)
+    time.sleep(1)
+    print("&:Hallo "+ Halil+ " Sorry ik ben nu heel erg bezig met iets?")
+    time.sleep(1)
+    print("#: Oke geen probleem.")
+    time.sleep(1)
+    print("&: wat is jou telefoonnummer?")
+    time.sleep(1)
+    print("#Mijn nummer is "+0615527384)
+    time.sleep(1)
+    print("&: dank je wel. Ik bel je wel later, doei!")
+    time.sleep(1)
+    print("#: Doei!")
+    time.sleep(1)
+    print("druk...")
