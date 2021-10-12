aa7cee448a32c12d46878ca4b8f4ededbc7191de
Halil Dogan <33289@ma-web.nl>
2021-10-12 10:17:17 +0200

    Nieuwe bestand

---
+++ PYTB1L4WheresWaldo.py
@@ -0,0 +1,3 @@
+import random
+people = ["Halil","Joeri","Mert","Roan","Milan","Braydon","Kiet","Rutger", "Bilal","Dion","Ivar","Amir","Judensio","Miyaki","Mavis", "Tom", "Abisha","Mike","Mitchel", "Jah"]
+random.shuffle(people)
