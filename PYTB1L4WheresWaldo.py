3ed732a16421cd9c00594f3af5f8097859a364a2
Halil Dogan <33289@ma-web.nl>
2021-10-26 10:28:39 +0200

    derde commit

--- PYTB1L4WheresWaldo.py
+++
@@ -1,3 +0,0 @@
-import random
-people = ["Halil","Joeri","Mert","Roan","Milan","Braydon","Kiet","Rutger", "Bilal","Dion","Ivar","Amir","Judensio","Miyaki","Mavis", "Tom", "Abisha","Mike","Mitchel", "Jah"]
-random.shuffle(people)
